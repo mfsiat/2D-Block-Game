6c12fcca16b66f6672386df35dea2200210649a7
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-04-21 01:57:43 +0600

    Done with the frame with JFrame

--- src/codes/Dodge.java
+++ src/codes/Dodge.java
@@ -1,8 +1,17 @@
 package codes;
 
+import java.awt.*;
 import javax.swing.JFrame;
 
+
 public class Dodge {
-	JFrame frame = new JFrame();
 	
+	public static void main(String [] args) {
+		JFrame frame = new JFrame();
+		frame.setTitle("Dodge the Rectangles");
+		frame.setSize(new Dimension(500, 400));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null); // centers the frame in the middle of the screen
+		frame.setVisible(true);
+	}
 }	
