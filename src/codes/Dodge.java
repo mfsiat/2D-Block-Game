0ce743a8f3f849c8bd52be83dd6c5c13a38148b5
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-04-21 02:12:40 +0600

    created the paint() method which helps us to draw

--- src/codes/Dodge.java
+++ src/codes/Dodge.java
@@ -6,10 +6,21 @@ import javax.swing.*;
 
 public class Dodge extends JPanel {
 	 
+	private int x = 5, y = 5;
+	
 	public Dodge() {
 		setSize(new Dimension(500, 400));
-		setPreferredSize(new Dimension(500, 400));
-		setBackground(Color.BLACK);
+		setPreferredSize(new Dimension(500, 400)); 
+	}
+	
+	// built in method that takes graphics g as perameter
+	// it helps to draw
+	public void paint(Graphics g) {
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		g.setColor(Color.WHITE);
+		repaint();
 	}
 	
 	public static void main(String [] args) {
@@ -19,7 +30,7 @@ public class Dodge extends JPanel {
 		frame.setTitle("Dodge the Rectangles");
 		frame.add(game);
 		frame.pack();
-		frame.setResizable(false);
+		frame.setResizable(false);   
 		frame.setSize(new Dimension(500, 400));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null); // centers the frame in the middle of the screen
