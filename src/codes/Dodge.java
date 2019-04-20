61316ef845fc436053b5a89fbec8a2b6078bc4d4
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-04-21 02:03:57 +0600

    Extended JPanel and created Dodge method

--- src/codes/Dodge.java
+++ src/codes/Dodge.java
@@ -1,14 +1,25 @@
 package codes;
 
 import java.awt.*;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 
-public class Dodge {
+public class Dodge extends JPanel {
+	 
+	public Dodge() {
+		setSize(new Dimension(500, 400));
+		setPreferredSize(new Dimension(500, 400));
+		setBackground(Color.BLACK);
+	}
 	
 	public static void main(String [] args) {
+		Dodge game = new Dodge();
+		
 		JFrame frame = new JFrame();
 		frame.setTitle("Dodge the Rectangles");
+		frame.add(game);
+		frame.pack();
+		frame.setResizable(false);
 		frame.setSize(new Dimension(500, 400));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null); // centers the frame in the middle of the screen
