4b1477202b3f25d0da777370e599bdd5ce485392
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-04-23 17:30:30 +0600

    Enemy class movement update

--- src/codes/Enemy.java
+++ src/codes/Enemy.java
@@ -13,11 +13,26 @@ public class Enemy extends Entity{
 	
 	public Enemy(Dodge instance, int x, int y) {
 		super(x, y);
+		this.instance = instance;
 		hitbox = new Rectangle(x, y, 64, 64);
+		
+		
+//		the rectangles will always come to downward
+		ix = 0;
+		iy = 1;
 	}
 	
 	public void move() {
-		
+		if(instance.getStage().isCollided(hitbox)) {
+			iy = 0;
+			dead = true;
+		}
+		hitbox.x += ix;
+		hitbox.y += iy;
+	}
+	
+	public boolean isDead() {
+		return dead;
 	}
 	
 	public void draw(Graphics g) {
